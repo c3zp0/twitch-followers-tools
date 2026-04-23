20f067f2dd775eba2adb914ecd5a2255fdea9ae1
siarhei <siarhei.me@gmail.com>
2026-04-23 11:32:33 +0300

    fix: ghost updates unfollowed users unfollow date

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -81,7 +81,7 @@ const updateFollowers = async (userId, actualFollowers) => {
         if (!isFollowerInMap) {
             storedFollowersMap.set(_follower.followerName, _follower);
         }
-        if (!isUserStillFollower && isUserStillFollower.status === 'follow') {
+        if (!isUserStillFollower && _follower.status !== 'unfollow') {
             _follower.status = 'unfollow';
             unfollowed.push(_follower);
         }
